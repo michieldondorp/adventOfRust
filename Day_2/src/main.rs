2be77ec83c5dc7ab9ab140a0e1e62b50a21b4931
Michiel Dondorp <m.dondorp@gmail.com>
2024-12-03 21:48:15 +0100

    Day2, 2nd star completed

--- Day_2/src/main.rs
+++ Day_2/src/main.rs
@@ -3,46 +3,65 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-
-    //read line
     if let Ok(lines) = read_lines("./input.txt") {
-        let total : i32 = lines.flatten()
-        .map(|line| -> i32 { 
+        let total : u16 = lines.flatten()
+        .map(|line| -> u16 { 
             let values: Vec<i32>= line.split_ascii_whitespace()
             .map(|value| -> i32 { value.parse().unwrap()})
             .collect();
             
-            let asc: bool = &values[0] < &values[1];
-            let mut valid: bool = true;
-
-            for value in 1..values.len() {
-                let cur = &values[value];
-                let prev = &values[value -1];
-                let diff = cur.abs_diff(*prev);
-                if diff < 1 || diff > 3 {
-                    valid = false;
-                    break;
-                }
-                if asc {
-                    if cur > prev {
-                        continue
-                    }
-                } else {
-                    if cur < prev {
-                        continue
-                    }
-                }
-                valid = false;
-                break;
-            }
-            if valid { 
-                return 1 
+            if is_valid_report(values, false) {
+                return 1;
             }
             0
         }
         ).sum();
-        println!("Valid {total}");
-    }    
+        println!("Safe reports: Valid {total}");
+    }
+    
+    if let Ok(lines) = read_lines("./input.txt") {
+        let total : u16 = lines.flatten()
+        .map(|line| -> u16 { 
+            let values: Vec<i32>= line.split_ascii_whitespace()
+            .map(|value| -> i32 { value.parse().unwrap()})
+            .collect();
+            
+            if is_valid_report(values, true) {
+                return 1;
+            }
+            0
+        }
+        ).sum();
+        println!("Safe reports with dampener: Valid {total}");
+    }  
+}
+
+fn is_valid_report(values : Vec<i32>, with_dampening :bool) -> bool{
+    if values.len() < 2 {
+        return false;
+    }
+    let asc: bool = &values[0] < &values[1];
+
+    for value in 1..values.len() {
+        let cur = &values[value];
+        let prev = &values[value -1];
+        let diff = cur.abs_diff(*prev);
+        if (diff > 0 && diff < 4) && (asc && cur > prev || !asc && cur < prev) {
+            continue
+        }
+        if with_dampening {
+            //values waarbij 1 waarde eruit is.
+            for subset in 0..values.len() {
+                let mut sub = values.clone();
+                sub.remove(subset);
+                if is_valid_report(sub, false) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    return true;
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
