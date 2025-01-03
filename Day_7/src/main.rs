878decb49bd8906e471e57a09a5f24ee0888823f
Michiel Dondorp <m.dondorp@gmail.com>
2025-01-03 17:14:25 +0100

    Day 7 star 2

--- Day_7/src/main.rs
+++ Day_7/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     path::Path,
 };
 
-#[derive(Debug)]
+#[derive(Clone)]
 struct Equation {
     total: i64,
     ints: Vec<i64>,
@@ -12,15 +12,52 @@ struct Equation {
 
 fn main() {
     let lines = read_lines("input.txt").ok().expect("To have read file");
-    let sum = lines.flatten().map(|line| -> Equation {
-        line_to_equation(line.clone())
-    }).filter( |eq| {
-        solve_with_operators(eq, 1, eq.ints[0])
-    }).map(|eq| {
-        eq.total
-    }).sum::<i64>();
+    let operators: Vec<Equation> = lines
+        .flatten()
+        .map(|line| -> Equation { line_to_equation(line.clone()) })
+        .collect();
 
-    println!("Star 1: {}", sum);
+    let sum1: i64 = operators
+        .clone()
+        .into_iter()
+        .filter(|eq| solve_with_operators(eq, 1, eq.ints[0]))
+        .map(|eq| eq.total)
+        .sum::<i64>();
+
+    let sum2: i64 = operators
+        .into_iter()
+        .filter(|eq| solve_with_operators_with_concat(eq.ints[0], &eq.ints.as_slice()[1..], eq.total))
+        .map(|eq| eq.total)
+        .sum::<i64>();
+
+    println!("Star 1: {}", sum1);
+    println!("Star 2: {}", sum2);
+}
+
+fn operator_concat(left: i64, right: i64) -> i64 {
+    let mut stringed_concat: String = left.to_string();
+    stringed_concat.push_str(&right.to_string());
+    stringed_concat
+        .parse()
+        .expect("input should really be an positive int")
+}
+
+fn solve_with_operators_with_concat(current_sum: i64, ints: &[i64], total: i64) -> bool {
+    if total < current_sum {
+        return false;
+    }
+    if ints.len() > 0 {
+        let next: i64 = ints[0];
+
+        if solve_with_operators_with_concat(current_sum + next, &ints[1..ints.len()], total) {
+            return true
+        }
+        if solve_with_operators_with_concat(current_sum * next, &ints[1..ints.len()], total) {
+            return true
+        }
+        return solve_with_operators_with_concat(operator_concat(current_sum, next), &ints[1..ints.len()], total);
+    }
+    return total == current_sum;
 }
 
 fn solve_with_operators(eq: &Equation, pos: i32, total: i64) -> bool {
@@ -31,18 +68,10 @@ fn solve_with_operators(eq: &Equation, pos: i32, total: i64) -> bool {
         return total == eq.total;
     }
 
-    if solve_with_operators(
-        eq,
-        pos + 1,
-        total + eq.ints[pos as usize]
-    ) {
+    if solve_with_operators(eq, pos + 1, total + eq.ints[pos as usize]) {
         return true;
     }
-    return solve_with_operators(
-        eq,
-        pos + 1,
-        total * eq.ints[pos as usize]
-    )
+    return solve_with_operators(eq, pos + 1, total * eq.ints[pos as usize]);
 }
 
 fn line_to_equation(input: String) -> Equation {
@@ -63,4 +92,25 @@ where
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(operator_concat(1, 2), 12);
+    }
+
+    #[test]
+    fn test_concat_2() {
+        assert_eq!(operator_concat(123, 456), 123456);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_concat_error() {
+        assert_eq!(operator_concat(123, -1), 123456);
+    }
 }
